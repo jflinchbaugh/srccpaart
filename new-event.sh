95ba771c4451b7c6fb2d55472b5e9c603a53f08b
John Flinchbaugh <john@hjsoft.com>
2024-08-01 18:46:46 -0400

    shorten up scripts, only 1 date

--- new-event.sh
+++ new-event.sh
@@ -1,37 +1,35 @@
 #!/bin/sh
 
-if [ "$#" -lt 8 ]; then
-    echo "Usage: $0 '<image_url>' '<event_url>' '<file_date>' '<event_expiration_date>' '<billboard_expiration_date>' '<title>' '<when>' '<description>'"
+if [ "$#" -lt 6 ]; then
+    echo "Usage: $0 '<image_url>' '<event_url>' '<date>' '<title>' '<when>' '<description>'"
     exit 1
 fi
 
 image_url=$1; shift
 event_url=$1; shift
-file_date=$1; shift
-event_expiration_date=$1; shift
-billboard_expiration_date=$1; shift
+date=$1; shift
 title=$1; shift
 when=$1; shift
 description=$1; shift
 
 lower_title=$(echo $title | tr '[A-Z] ' '[a-z]-' | sed 's/[^a-z0-9-]//g')
-file_name="${file_date}-${lower_title}"
+file_name="${date}-${lower_title}"
 
-curl "$image_url" > "/tmp/$$.jpg"
+curl "$image_url" > "/tmp/$$.jpg" || exit 1
 cp "/tmp/$$.jpg" "static/billboard/${file_name}.jpg"
 cp "/tmp/$$.jpg" "static/events/${file_name}.jpg"
 rm -f "/tmp/$$.jpg"
 
 cat > "data/billboard/${file_name}.yaml" <<EOF
 href: ${file_name}.jpg
-expiration: ${billboard_expiration_date}
+date: ${date}
 EOF
 
 cat > "data/events/${file_name}.yaml" <<EOF
 title: "${title}"
 when: "${when}"
 image: "/events/${file_name}.jpg"
-expiration: ${event_expiration_date}
+date: ${date}
 link: "${event_url}"
 actionText: RSVP and share on Facebook
 description: |
